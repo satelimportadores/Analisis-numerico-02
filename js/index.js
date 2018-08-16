6785e498f7dacfc8118c4969e01ad539095a9608
Juan <satelimportadores@gmail.com>
2018-08-16 09:09:47 -0500

    Inicio elementos requeridos

---
+++ js/index.js
@@ -0,0 +1,31 @@
+//Variables para texto
+  var mensaje_bin = "";
+  var mensaje_oct = "";
+  var mensaje_hex = "";
+//Variables para texto
+
+$(document).on('ready',function(){
+
+  //Esconder errores
+  $('.invalid-numero').hide();
+  $('.invalid-base').hide();
+  //Esconder errores
+
+  //Boton calcular
+    $( "#calcular" ).click(function() {
+      
+    });
+  //Boton calcular
+
+});
+
+  var validar = function(n,b){
+
+    }
+
+  var calcular = function(n,b){ 
+
+  }
+
+
+
